fe21e154c4b8facb3151edbf8cb1ba10197a74ec
suikasa <2570005408@qq.com>
2019-08-22 15:07:03 +0800

    第一次提交

---
+++ src/main/java/suikasa/new0822/New0822Application.java
@@ -0,0 +1,13 @@
+package suikasa.new0822;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class New0822Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(New0822Application.class, args);
+    }
+
+}
---
+++ src/test/java/suikasa/new0822/New0822ApplicationTests.java
@@ -0,0 +1,16 @@
+package suikasa.new0822;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class New0822ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
